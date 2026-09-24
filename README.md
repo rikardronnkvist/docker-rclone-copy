3da1cdfb24670cdf1cd8af775211f325d5135aee
rikardronnkvist <rikard@ronnkvist.nu>
2026-09-24 10:55:31 +0200

    Improve healthcheck curl handling

--- README.md
+++ README.md
@@ -29,7 +29,7 @@ A few environment variables allow you to customize the behavior of the copy:
 * `FORCE_COPY` set variable to perform a copy upon boot
 * `COPY_OPTS` additional options for `rclone copy` command. Defaults to `-v`
 * `TZ` set the [timezone](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones) to use for the cron and log
-* `CHECK_URL` [healthchecks.io](https://healthchecks.io) url or similar cron monitoring to perform a `POST` before and after a copy
+* `CHECK_URL` [healthchecks.io](https://healthchecks.io) url or similar cron monitoring to perform curl before and after a copy (no trailing slash)
 
 
 ```bash
--- copy.sh
+++ copy.sh
@@ -9,30 +9,32 @@ then
   echo "WARNING: A previous copy is still running. Skipping new copy command."
 else
 
-echo $$ > /tmp/copy.pid
+  echo $$ > /tmp/copy.pid
 
-if test "$(rclone ls $COPY_SRC $RCLONE_OPTS)"; then
-  if [ -z "$CHECK_URL" ]
-  then
-    echo "INFO: Define CHECK_URL with https://healthchecks.io to monitor sync job"
+  if test "$(rclone ls $COPY_SRC $RCLONE_OPTS)"; then
+    if [ -z "$CHECK_URL" ]
+    then
+      echo "INFO: Define CHECK_URL with https://healthchecks.io to monitor sync job"
+    else
+      echo "INFO: Sending start ping to $CHECK_URL/start"
+      curl -fsS -m 10 --retry 5 "$CHECK_URL/start" > /dev/null
+    fi
+
+    # the source directory is not empty
+    # it can be synced without clear data loss
+    echo "INFO: Starting rclone copy $COPY_SRC $COPY_DEST $RCLONE_OPTS $COPY_OPTS"
+    rclone copy $COPY_SRC $COPY_DEST $RCLONE_OPTS $COPY_OPTS
+
+    if [ -n "$CHECK_URL" ]
+    then
+      echo "INFO: Sending ping to $CHECK_URL"
+      curl -fsS -m 10 --retry 5 "$CHECK_URL" > /dev/null
+    fi
+
   else
-    curl -fsS --max-time 10 -X POST -H 'Content-Type: text/plain' --data-raw "start" "$CHECK_URL"
+    echo "WARNING: Source directory is empty. Skipping copy command."
   fi
 
-  # the source directory is not empty
-  # it can be synced without clear data loss
-  echo "INFO: Starting rclone copy $COPY_SRC $COPY_DEST $RCLONE_OPTS $COPY_OPTS"
-  rclone copy $COPY_SRC $COPY_DEST $RCLONE_OPTS $COPY_OPTS
-
-  if [ -n "$CHECK_URL" ]
-  then
-    curl -fsS --max-time 10 -X POST -H 'Content-Type: text/plain' --data-raw "success" "$CHECK_URL"
-  fi
-
-else
-  echo "WARNING: Source directory is empty. Skipping copy command."
-fi
-
-rm -f /tmp/copy.pid
+  rm -f /tmp/copy.pid
 
 fi
